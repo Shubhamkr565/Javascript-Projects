e755839fa25b00b38e0927b0bc84357cdcad1515
Shubhamkr565 <140379407+Shubhamkr565@users.noreply.github.com>
2023-08-01 11:52:44 +0530

    countdown timer

---
+++ countdown timer/script.js
@@ -0,0 +1,29 @@
+const endDate = "2023-08-01T11:42:56";
+document.getElementById("end-Date").innerText = endDate;
+
+const inputs = document.querySelectorAll("input")
+
+ function clock() {
+    const end = new Date(endDate)
+    const now = new Date()
+    const diff = (end - now)/ 1000;
+
+    if(diff<0)return;
+
+    //convert into day; 
+    inputs[0].value =(Math.floor(diff / 3600  / 24));
+    inputs[1].value = Math.floor(diff / 3600) % 24;
+    inputs[2].value = Math.floor(diff / 60) % 60;
+    inputs[3].value = Math.floor(diff) % 60;
+     
+    
+}
+
+clock()
+
+setInterval(
+    () => {
+        clock()
+    },
+    1000
+)
